0461f1e975578e5d4c25f9fa30a4d3786e859978
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-01 05:51:16 +0200

    Final commit

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -1,7 +1,25 @@
 const check = document.getElementById("checkbox");
+const color = document.getElementById("task");
+const bton = document.getElementById("task-button");
+const task = document.getElementById("added-task");
+
+const taskList = ["walk the dog"]
+
 check.addEventListener("click", function(){
-    
+   if(check==true){
+     add color="red" to div class
+     }
+   
+bton.addEventListener("click", function () {
+    if( usernameInput.value == "AdminSEF123" && passwordInput.value == "SeF@ctORy$$456"){
+        window.location.href = "../Pages/todo.html"
+    }
+    else{
+        alert("User does not exist. Please enter a valid username ")
+    }
+});
 
 
-} 
-);
+  
+
+
